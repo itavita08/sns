8aecf1e048bb6cc735e06e36d8269e4c08ea00db
itavita08 <dudwns10000@gmail.com>
2023-08-29 03:16:40 +0900

    authentication
    Spring Security는 세션에 객체를 authentication타입으로 저장해주고 authentication은
    User객체(UserDetail 타입)를 저장하기 떄문에
    authentication.getName() 으로 userName을 받으려면 User객체는 UserDetails를 상속받아야함으로 수정
    
    (1) https://velog.io/@sally_devv/JWT-%ED%86%A0%ED%81%B0%EB%A7%8C-%EC%9D%B4%EC%9A%A9%ED%95%B4%EB%B3%B4%EB%A0%A4-%ED%95%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8
    
    (2) https://ws-pace.tistory.com/249

--- src/main/java/com/example/sns/controller/PostController.java
+++ src/main/java/com/example/sns/controller/PostController.java
@@ -20,7 +20,7 @@ public class PostController {
     @PostMapping
     public Response<Void> create(@RequestBody PostCreateRequest request, Authentication authentication){
 
-        postService.create(request.getTitle(), request.getBody(), "");
+        postService.create(request.getTitle(), request.getBody(), authentication.getName());
 
         return Response.success(null);
     }
