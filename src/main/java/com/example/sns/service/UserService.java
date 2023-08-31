f10c6f4112142230caccfdb9f572f9c79097afd0
itavita08 <dudwns10000@gmail.com>
2023-08-31 23:52:42 +0900

    Redis 연동
    Heroku Redis 생성 후 연동
    User데이터를 캐싱
    User 데이터 레디스 연동 후 확인 완료

--- src/main/java/com/example/sns/service/UserService.java
+++ src/main/java/com/example/sns/service/UserService.java
@@ -6,6 +6,7 @@ import com.example.sns.model.Alarm;
 import com.example.sns.model.User;
 import com.example.sns.model.entity.UserEntity;
 import com.example.sns.repository.AlarmEntityRepository;
+import com.example.sns.repository.UserCacheRepository;
 import com.example.sns.repository.UserEntityRepository;
 import com.example.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,7 @@ public class UserService {
     private final UserEntityRepository userEntityRepository;
     private final AlarmEntityRepository alarmEntityRepository;
     private final BCryptPasswordEncoder encoder;
+    private final UserCacheRepository userCacheRepository;
 
     @Value("${jwt.secret-key}")
     private String secretKey;
@@ -32,14 +34,13 @@ public class UserService {
     private long expiredTimeMs;
 
     public User loadUserByUserName(String userName){
-
-        return userEntityRepository.findByUserName(userName).map(User :: fromEntity)
-                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        return userCacheRepository.getUser(userName).orElseGet(
+                () -> userEntityRepository.findByUserName(userName).map(User :: fromEntity)
+                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName))));
     }
 
     @Transactional
     public User join(String userName, String password){
-
         userEntityRepository.findByUserName(userName).ifPresent(it -> {
             throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userName));
         });
@@ -50,24 +51,17 @@ public class UserService {
     }
 
     public String login(String userName, String password){
+        User user = loadUserByUserName(userName);
+        userCacheRepository.setUser(user);
 
-        UserEntity userEntity = userEntityRepository.findByUserName(userName)
-                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
-
-        if(!encoder.matches(password, userEntity.getPassword())){
+        if(!encoder.matches(password, user.getPassword())){
             throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
-        String token = JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
-
-        return token;
+        return JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
     }
 
     public Page<Alarm> alarmList(Integer userId, Pageable pageable) {
-
-//        UserEntity userEntity = userEntityRepository.findByUserName(userName)
-//                .orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
-
         return alarmEntityRepository.findAllByUserId(userId, pageable).map(Alarm::fromEntity);
     }
 }
